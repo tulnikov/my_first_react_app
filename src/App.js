3e8685719ae1b31117879b8b96c437c867fca3b6
tulnikov <dima@tulnikov.ru>
2024-07-04 15:30:02 +0300

    Забыл

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import Table from "./components/Table";
 import Form from "./components/Form";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import "./App.css";
 import persons from "./data/persons";
 
 
 function App(props) {
+
+    const API_URL = 'https://jsonplaceholder.typicode.com/posts'
     const [DB, setDB] = useState(persons)
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        fetch(API_URL)
+            .then(response => response.json())
+            .then(json => console.log(json))
+            .finally(setIsLoading(false))
+
+    }, []);
+
 
     function handlerBDChange(input) {
-        const count = DB.length+1
+        const count = DB.length + 1
         setDB(
             [...DB, {
-                id:count,
+                id: count,
                 firstName: input.firstName.value,
                 lastName: input.lastName.value,
                 gender: input.gender.value,
@@ -20,20 +32,29 @@ function App(props) {
                 phone: input.phone.value
             }]
         )
+
+
     }
 
     const clearTable = () => {
-
         setDB([])
     }
 
-    return (
-        <div className="App">
-            <Form onSubmit={handlerBDChange} count={DB.length}  clTbl={clearTable}/>
-            <Table mydb={DB}/>
 
+
+    return (
+
+        <div className="App">
+
+            {isLoading ? <h1>Loading...</h1> :
+                <>
+                    <Form onSubmit={handlerBDChange} count={DB.length} clTbl={clearTable}/>
+                    <Table mydb={DB}/>
+                </>}
         </div>
     );
+
+
 }
 
 export default App;
